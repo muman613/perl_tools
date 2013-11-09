be2b8fbd672595e491682c7a497a7e51df399249
Michael Uman <Michael_Uman@sigmadesigns.com>
2013-11-08 16:05:01 -0800

    Adding subroutines

--- make_cbp/mkcbp.pl
+++ make_cbp/mkcbp.pl
@@ -7,11 +7,38 @@
 
 use XML::LibXML;
 
+
+my $project_title = "Sample_Project";
+
+##############################################################################
+#   Add an 'Option' node to the passed $node
+##############################################################################
+
+sub add_option($$$$) {
+    my $doc = shift;
+    my $node = shift;
+    my $optname = shift;
+    my $optval = shift;
+    my $newNode = undef;
+    
+    $newNode = $doc->createElement("Option");
+    $newNode->addChild( $doc->createAttribute($optname, $optval) );
+    $node->addChild($newNode);
+    
+    return;
+}
+
+##############################################################################
+#   Begin
+##############################################################################
+
 my $doc = XML::LibXML::Document->new("1.0", "UTF-8");
 my $root = $doc->createElement("CodeBlocks_project_file");
 my $node;
-my $proj, $build, $target, $comp, $link, $unit; # project node
+my ($proj, $build, $target, $comp, $link, $unit); # project node
+
 $doc->setDocumentElement($root);
+$doc->setStandalone(1);
 
 $node = $doc->createElement("FileVersion");
 $node->addChild( $doc->createAttribute( major => 1 ) );
@@ -21,17 +48,9 @@ $root->addChild($node);
 
 $proj = $doc->createElement("Project");
 
-$node = $doc->createElement("Option");
-$node->addChild( $doc->createAttribute( title => ShitRaker ) );
-$proj->addChild($node);
-
-$node = $doc->createElement("Option");
-$node->addChild( $doc->createAttribute( pch_mode => 2 ) );
-$proj->addChild($node);
-
-$node = $doc->createElement("Option");
-$node->addChild( $doc->createAttribute( compiler => gcc ) );
-$proj->addChild($node);
+add_option($doc, $proj, "title", $project_title);
+add_option($doc, $proj, "pch_mode", "2");
+add_option($doc, $proj, "compiler", "gcc");
 
 $build = $doc->createElement("Build");
 
